b7d2afb18573673ff9f06bace99eefdbcdecd790
AbhignaReddy10 <nabhigna1234@gmail.com>
2024-09-29 17:33:12 +0530

    Create easy.java

---
+++ easy.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Easy {
+    public static void main(String[] args)
+    {
+        // creating the instance of class BufferedReader
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+        String name;
+        try {
+            System.out.println("Enter your name");
+            name = reader.readLine(); // taking string input
+            System.out.println("Name=" + name);
+        }
+    
+        }
+    }
+}
