fb80a7da1c913bce603ca609fc14fea275bee145
AbhignaReddy10 <nabhigna1234@gmail.com>
2024-12-05 14:53:36 +0530

    Add files via upload

---
+++ LoopArray.java
@@ -0,0 +1,11 @@
+public class LoopArray {
+public static void main(String[]args) {
+
+    String[] flowers= {"rose","lily","tulip"};
+    for (int i=0; i<flowers.length; i++)
+     {
+    System.out.println(flowers[i]);
+    }
+}
+    
+}
