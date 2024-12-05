b04e2aeb6ac7d1edfdbec66a10e7a2b190ee64dd
AbhignaReddy10 <nabhigna1234@gmail.com>
2024-12-05 14:31:56 +0530

    BinaryInt.java

---
+++ BinaryInt.java
@@ -0,0 +1,16 @@
+ import java.util.Scanner;
+public class DecimalBinary
+{
+public static String toBinary(int decimal)
+{
+return Integer.toBinaryString(decimal);
+}
+public static void main(String[] args)
+{
+Scanner scanner= new Scanner(System.in);
+System.out.print("enter decimal");
+int decimal=scanner.nextInt();
+System.out.println("binary" + toBinary(decimal));
+scanner.close();
+}
+}
