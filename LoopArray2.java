fca0db1786bda16920e5d74eef534a8261c27bc6
AbhignaReddy10 <nabhigna1234@gmail.com>
2024-12-05 15:06:06 +0530

    Add files via upload

---
+++ LoopArray2.java
@@ -0,0 +1,10 @@
+ public class LoopArray2{
+    public static void main(String[]args) {
+    String[] riverdale = {"betty","jughead","archie","veronica"};
+    for(String i:riverdale)
+    {
+        System.out.println(i);
+    }
+    }
+    
+}
