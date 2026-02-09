6dc2c9b6844f6655acf5ab7b26b6993ae5ef5a81
AbhignaReddy10 <nabhigna1234@gmail.com>
2026-02-09 21:09:28 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,21 +1,17 @@
+// this program is to check if teh given number is prime or not
+//it takes in the user input and runs the for loop to get the answer.
 import java.util.Scanner;
 class Solution{
-    public int Absolute(int n){
-        if (n<0){
-            return -n;
-            
-        }else{
-            return n;
-        }
-    }
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        if (sc.hasNextInt()){
-            int IN = sc.nextInt();
-            Solution sol = new Solution();
-            int result = sol.Absolute(IN);
-            System.out.println(result);
-        }
-    }
+public Boolean isPrime(int n){
+if (n==1)return false;
+for(int I =2;i*i<=n;i++){
+if (n%1==0) return false;
 }
-
+return true;
+}
+public static void main(String [[]args){
+Scanner sc = new Scanner(System.in);
+int num = sc.nextInt();
+Solution sol = new Solution();
+Boolean result = sol.isPrime(num);
+System.out.println(result);
